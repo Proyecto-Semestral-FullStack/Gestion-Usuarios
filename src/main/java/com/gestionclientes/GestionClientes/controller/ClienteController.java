e966201f011839a89b95518ebff953a0f1f360e3
Gerard419 <ge.foix@duocuc.cl>
2026-05-04 11:39:26 -0400

    metodo actualizar agregado en el Controlador

---
+++ src/main/java/com/gestionclientes/GestionClientes/controller/ClienteController.java
@@ -0,0 +1,44 @@
+package com.gestionclientes.GestionClientes.controller;
+
+import com.gestionclientes.GestionClientes.dto.ClienteRequestDTO;
+import com.gestionclientes.GestionClientes.dto.ClienteResponseDTO;
+import com.gestionclientes.GestionClientes.service.ClienteService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/clientes")
+@RequiredArgsConstructor
+public class ClienteController{
+
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDTO>> obtenerClientes(){
+        return ResponseEntity.ok(clienteService.obtenerTodos());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ClienteResponseDTO> obtenerPorId(@RequestParam(name = "id",required = true) Long id){
+        return clienteService.obtenerPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("{nombre}")
+    public ResponseEntity<ClienteResponseDTO> obtenerPorId(@RequestParam(name = "nombre",required = true) String nombre){
+        return clienteService.obtenerPorNombre(nombre).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<ClienteResponseDTO> crear(@Valid @RequestBody ClienteRequestDTO dto){
+        return ResponseEntity.status(201).body(clienteService.guardar(dto));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ClienteResponseDTO> actualizar(@RequestParam(name = "id",required = true) Long id,@Valid @RequestBody ClienteRequestDTO dto){
+        return clienteService.actualizar(id,dto).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+}
