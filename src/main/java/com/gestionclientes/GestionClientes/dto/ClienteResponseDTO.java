68d07bb48931ed18e7e4d3af40d2b9985f08d5d5
Gerard419 <ge.foix@duocuc.cl>
2026-05-17 17:12:02 -0400

    actualizacion atributos del dto ClienteResponse
    Added new fields for 'apellido', 'activo', 'imagenId', 'fechaCreacion', and 'fechaActualizacion'. Updated 'rol' type from String to Long.

--- src/main/java/com/gestionclientes/GestionClientes/dto/ClienteResponseDTO.java
+++ src/main/java/com/gestionclientes/GestionClientes/dto/ClienteResponseDTO.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClienteResponseDTO {
     private Long id;
     private String nombre;
+    private String apellido;
     private String correo;
-    private String rol;
     private String contrasena;
+    private Long rol;
+    private Boolean activo;
+    private Long imagenId;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaActualizacion;
 }
