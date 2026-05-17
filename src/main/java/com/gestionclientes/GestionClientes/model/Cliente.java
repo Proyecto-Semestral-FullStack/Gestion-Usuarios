50291d795021800e66db956477791b50389e8a56
Gerard419 <ge.foix@duocuc.cl>
2026-05-17 16:31:51 -0400

    actualizacion atributos del modelo Cliente

--- src/main/java/com/gestionclientes/GestionClientes/model/Cliente.java
+++ src/main/java/com/gestionclientes/GestionClientes/model/Cliente.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table(name = "clientes")
+@Table(name = "usuarios")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,15 +17,31 @@ public class Cliente{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 40, unique = true)
+    @Column(nullable = false, length = 100)
     private String nombre;
 
-    @Column(nullable = false, length = 60, unique = true)
-    private String correo;
+    @Column(length = 100)
+    private String apellido;
 
-    @Column(nullable = false, length = 20)
-    private String rol;
+    @Column(nullable = false, length = 150, unique = true)
+    private String correo;
 
     @Column(nullable = false, length = 50)
     private String contrasena;
+
+    @Column(nullable = false)
+    private Long rol;
+
+    @Column(nullable = false)
+    private Boolean activo;
+
+    @Column
+    private Long imagen_id;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaActualizacion;
+
 }
