63ac4f911045339aa0fb63c3a534e8794421e643
Gerard419 <ge.foix@duocuc.cl>
2026-05-17 23:33:07 -0400

    insercion del BCryptPqasswordEncoder para encriptar las contraseñas de los datos de prueba

--- src/main/java/com/gestionclientes/GestionClientes/config/DataInitializer.java
+++ src/main/java/com/gestionclientes/GestionClientes/config/DataInitializer.java
@@ -5,6 +5,7 @@ import com.gestionclientes.GestionClientes.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner{
     private final ClienteRepository clienteRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,8 +25,8 @@ public class DataInitializer implements CommandLineRunner{
         }
         LocalDateTime ahora = LocalDateTime.now();
         log.info(">>>DataInitializer: BD vacía detectada, insertando datos de prueba...");
-        clienteRepository.save(new Cliente(null,"Javier","Rodriguez","javiR@gmail.com","Jav2000",1L,true,null,ahora,ahora));
-        clienteRepository.save(new Cliente(null,"Sopaipilla69",null,"sop69@gmail.com","S6opaipill9a.",2L,true,null,ahora,ahora));
+        clienteRepository.save(new Cliente(null,"Javier","Rodriguez","javiR@gmail.com",passwordEncoder.encode("Jav2000"),1L,true,null,ahora,ahora));
+        clienteRepository.save(new Cliente(null,"Sopaipilla69",null,"sop69@gmail.com",passwordEncoder.encode("S6opaipill9a"),2L,true,null,ahora,ahora));
         log.info(">>> DataInitializer: usuarios insertados correctamente",clienteRepository.count());
     }
 }
