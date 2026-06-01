47102a9c7a08b48a0f80f19f74bafa5e0a6136ce
devroa <roa.alejandra02@gmail.com>
2026-06-01 14:42:08 -0400

    se agrega la anotacion @EnableDiscoveryClient para que eureka pueda descubrirlo y pueda utilizarse para que mejore seguridad  en los endpoints a traves de Gateway que se comunica con los otros microservicios ya que se necesita estar autenticado como usuario para acceder a ciertos endpoints en las peticiones ADMIN Y USUARIO_CLIENTE tienen diferentes permisos.Ej :un usuario cliente no puede crear un producto porque no es quien vende si no quien compra el producto

--- src/main/java/com/gestionclientes/GestionClientes/GestionClientesApplication.java
+++ src/main/java/com/gestionclientes/GestionClientes/GestionClientesApplication.java
@@ -2,8 +2,10 @@ package com.gestionclientes.GestionClientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestionClientesApplication {
 
 	public static void main(String[] args) {
