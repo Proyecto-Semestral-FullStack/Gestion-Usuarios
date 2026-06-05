2eab93b6e7c59411aaed486865e78e82820216db
Gerard419 <ge.foix@duocuc.cl>
2026-06-05 00:24:55 -0400

    Se crearon 3 clases de excepciones personalizadas , validacion de datos de usuario, usuario no encontrado y usuario ya existente

---
+++ src/main/java/com/gestionclientes/GestionClientes/exception/CreadencialesInvalidasException.java
@@ -0,0 +1,7 @@
+package com.gestionclientes.GestionClientes.exception;
+
+public class CreadencialesInvalidasException extends RuntimeException {
+    public CreadencialesInvalidasException(String message) {
+        super(message);
+    }
+}
