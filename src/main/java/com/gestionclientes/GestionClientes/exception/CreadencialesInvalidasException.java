5e0bdbc8c5ded05263adef7b283467e79bf5c1ad
Gerard419 <ge.foix@duocuc.cl>
2026-06-09 11:29:08 -0400

    Renombrado el archivo de Creadenciales a Credenciales

--- src/main/java/com/gestionclientes/GestionClientes/exception/CreadencialesInvalidasException.java
+++
@@ -1,7 +0,0 @@
-package com.gestionclientes.GestionClientes.exception;
-
-public class CreadencialesInvalidasException extends RuntimeException {
-    public CreadencialesInvalidasException(String message) {
-        super(message);//
-    }
-}
